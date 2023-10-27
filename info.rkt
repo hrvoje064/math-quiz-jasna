4f5801d1817ffbee86a52ee1b918a23ad3a5f127
Hrvoje Blazevic <hrvoje064@gmail.com>
2023-10-27 13:02:49 +0800

    Fixed info.rkt file

--- info.rkt
+++ info.rkt
@@ -2,9 +2,13 @@
 (define name "Math-quiz")
 (define gracket-launcher-names '("Racket Math Quiz"))
 (define collection "math-quiz")
-(define deps '("base"))
+(define deps '("drracket"
+               "gui-lib"
+               "htdp-lib"
+               "pict-lib"
+               "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/math-quiz.scrbl" ())))
+(define scribblings '(("scribblings/math-quiz.scrbl")))
 (define pkg-desc "Math quiz for grade 1,2,3 ...")
 (define version "1.0")
 (define pkg-authors '(Hrvoje Blazevic))
